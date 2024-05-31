f026d91eb69722e72d10bd53401c4c2b348654b6
Jedy <johnericssonytac@gmail.com>
2024-05-31 10:45:50 +0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Nigga</h1>
-    </div>
+    <>
+      <aside>
+        <img src="" alt="Profile-pic" />
+        <h1 className='Name'>Jhon Ericsson Ytac</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About Me</a>
+            </li>
+            <li>
+              <a href="#">Education Background</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <footer><button>Hire Me</button></footer>
+      </aside>
+      <main></main>
+    </>
   );
 }
 
